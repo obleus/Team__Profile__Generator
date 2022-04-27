70cb11537f24bdb61e4f28dfcf7299d9eb81f711
obleus <odney@Odneys-MacBook-Air.local>
2022-04-27 14:59:11 -0400

    updated index.js / also included intern data

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site.js');
-
 const promptManager = () => {
     return inquirer.prompt([
         {
@@ -20,12 +18,12 @@ const promptManager = () => {
         {
             type: 'input',
             name: 'employeeID',
-            message: 'Enter your employee ID (Required)',
+            message: 'Enter  employee ID (Required)',
             validate: employeeId => {
                 if (employeeId) {
                     return true;
                 } else {
-                    console.log('Please enter your employee ID!');
+                    console.log('Please enter employee ID!');
                     return false;
                 }
             }
@@ -33,12 +31,12 @@ const promptManager = () => {
         {
             type: 'input',
             name: 'email',
-            message: 'Enter your email address (Required)',
+            message: 'Enter email address (Required)',
             validate: email => {
                 if (email) {
                     return true;
                 } else {
-                    console.log('Please enter your email address!');
+                    console.log('Please enter  email address!');
                     return false;
                 }
             }
@@ -51,10 +49,11 @@ const promptManager = () => {
                 if (officeNumber) {
                     return true;
                 } else {
-                    console.log('Please enter your office number!');
+                    console.log('Please enter  office number!');
                     return false;
                 }
             }
+        
         },
         {
             type: 'list',
@@ -64,97 +63,170 @@ const promptManager = () => {
         }
     ]);
 };
-
 const promptEmployee = employeeData => {
     console.log(`
     ===============
     Add a New Employee
     ===============
     `);
-
-    // If there's no 'employees' array property, create one
-    if (!employeeData.employees) {
-        employeeData.employees = [];
+// If there's no 'employees' array property, create one
+if (!employeeData.employees) {
+    employeeData.employees = [];
+}
+return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of engineer? (Required)',
+        validate: engineerName => {
+            if (engineerName) {
+                return true;
+            } else {
+                console.log('Please enter the name of engineer!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'Enter  employee ID (Required)',
+        validate: employeeId => {
+            if (employeeId) {
+                return true;
+            } else {
+                console.log('Please enter employee ID!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address (Required)',
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log('Please enter  email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter  Github username. (Required)',
+        validate: githubUsername => {
+            if (githubUsername) {
+                return true;
+            } else {
+                console.log('Please enter Github username!');
+                return false;
+            }
+        }
     }
+]).then(employeeData => {
+    employeeData.employees.push(employeeData);
+    if (employeeData.confirmAddEmployee) {
+        return promptProject(employeeData);
+    } else {
+        return employeeData;
+    }
+});
+};
+const promptIntern = internData => {
+console.log(`
+===============
+Add a New Intern
+===============
+`);
+if (!internData.interns) {
+    internData.interns = [];
+}
 
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'engineerName',
-            message: 'What is the name of engineer? (Required)',
-            validate: engineerName => {
-                if (engineerName) {
-                    return true;
-                } else {
-                    console.log('Please enter the name of engineer!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'employeeID',
-            message: 'Enter your employee ID (Required)',
-            validate: employeeId => {
-                if (employeeId) {
-                    return true;
-                } else {
-                    console.log('Please enter your employee ID!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email address (Required)',
-            validate: email => {
-                if (email) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'githubUsername',
-            message: 'Enter your Github username. (Required)',
-            validate: githubUsername => {
-                if (githubUsername) {
-                    return true;
-                } else {
-                    console.log('Please enter your Github username!');
-                    return false;
-                }
+return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of the intern? (Required)',
+        validate: internName => {
+            if (internName) {
+                return true;
+            } else {
+                console.log('Please enter the name of the intern!');
+                return false;
             }
         }
-    ]).then(employeeData => {
-        employeeData.employees.push(employeeData);
-        if (employeeData.confirmAddEmployee) {
-            return promptProject(employeeData);
-        } else {
-            return employeeData;
+    },
+    {
+        type: 'input',
+        name: 'employeeID',
+        message: 'Enter employee ID (Required)',
+        validate: employeeId => {
+            if (employeeId) {
+                return true;
+            } else {
+                console.log('Please enter  employee ID!');
+                return false;
+            }
         }
-    });
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address (Required)',
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your Github username. (Required)',
+        validate: githubUsername => {
+            if (githubUsername) {
+                return true;
+            } else {
+                console.log('Please enter your Github username!');
+                return false;
+            }
+        }
+    }
+]).then(internData => {
+    internData.interns.push(internData);
+    if (internData.confirmAddIntern) {
+        return promptIntern(internData);
+    } else {
+        return internData;
+    }
+});
 };
 
 promptManager()
-    .then(promptEmployee)
-    .then(employeeData => {
-        return generatePage(employeeData);
-    })
-    .then(pageHTML => {
-        return writeFile(pageHTML);
-    })
-    .then(writeFileResponse => {
-        console.log(writeFileResponse);
-        return copyFile();
-    })
-    .then(copyFileResponse => {
-        console.log(copyFileResponse);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+.then(promptEmployee)
+.then(employeeData => {
+    return generatePage(employeeData);
+})
+.then(promptInter)
+.then(internData => {
+    return generatePage(internData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
+});
