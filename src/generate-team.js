0e8e4171c0773247348cb1bfe60ca0f8993e3eeb
obleus <odney@Odneys-MacBook-Air.local>
2022-04-27 21:42:46 -0400

    update the generate site

--- src/generate-team.js
+++ src/generate-team.js
@@ -1,8 +1,31 @@
+const Manager = require("../lib/Manager");
+
 const generateTeam = (team) => {
+    console.log(team);
+  
+    const html = [];
+
+    const generateManager = manager => {
+        console.log(manager);
+        let managerHtml = ` <div class="card employee-card">
+           <p>ManagerInfo</p>
+        </div>
+        `;
+        html.push(managerHtml);
+    }
+
+   
+    for (let i = 0; i < team.length; i++) {
+        if (team[i].getRole() === "Manager") {
+            generateManager(team[i]);
+        }
+    }
+
+    // join the HTML blocks
+    return html.join('');
 }
 
 module.exports = team => {
-
     return `
     <!DOCTYPE html>
 <html lang="en">
